79441e3869eec8ed89de6d9e14698c94d8367579
magnusmoeheide <magnus.heide@code.berlin>
2023-04-29 21:28:55 +0200

    / remove

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const articleSectionsRoutes = require('./src/articleSections/article_sections_ro
 app.use(express.json())
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://magnusmoeheide.github.io/the-watoto-library-website/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://magnusmoeheide.github.io/the-watoto-library-website');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
