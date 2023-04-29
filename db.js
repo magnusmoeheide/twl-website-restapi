cfd614a99f03fd7792798702906154b4274d591a
magnusmoeheide <magnus.heide@code.berlin>
2023-04-29 19:15:48 +0200

    database connection modified

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 const Pool = require('pg').Pool
+
 const pool = new Pool({
   user: 'doadmin',
   host: 'db-postgresql-fra1-32058-do-user-14018081-0.b.db.ondigitalocean.com',
   database: 'defaultdb',
   password: 'AVNS_0biH4F6ZTJJ0rnfeXk7',
   port: 25060,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
