61017e7af9ef37b3bd6a79d4119e05d6c50121e0
magnusmoeheide <magnus.heide@code.berlin>
2023-04-29 18:22:07 +0200

    db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'superuser',
-  host: 'localhost',
-  database: 'twl_db',
-  password: 'superuser',
-  port: 5432,
+  user: 'doadmin',
+  host: 'db-postgresql-fra1-32058-do-user-14018081-0.b.db.ondigitalocean.com',
+  database: 'defaultdb',
+  password: 'AVNS_0biH4F6ZTJJ0rnfeXk7',
+  port: 25060,
 });
 
 module.exports = pool;
