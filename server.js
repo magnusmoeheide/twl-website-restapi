0eef9684aa5b6f4f479447726a11361fc5fc44ba
magnusmoeheide <magnus.heide@code.berlin>
2023-04-29 19:03:32 +0200

    Add ip address to server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const articleSectionsRoutes = require('./src/articleSections/article_sections_ro
 app.use(express.json())
 
 app.use(function (req, res, next) {
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  res.setHeader('Access-Control-Allow-Origin', 'http://165.22.84.23/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
 
